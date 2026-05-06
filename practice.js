515463a82ccc37308225e7a937dfde7fcd2a4a5f
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-05-06 20:41:03 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,25 @@
+
+
+(function Dbconn(){
+    console.log("Connected to the DB");
+})();  
+
+
+
+
+((connNum) => {
+    console.log(`${connNum} connection to the DB `);
+})("2nd");
+
+
+
+(function Dbconn(){
+    console.log("Connected to the DB");
+})();  
+
+
+
+
+((connNum) => {
+    console.log(`${connNum} connection to the DB `);
+})("2nd");
